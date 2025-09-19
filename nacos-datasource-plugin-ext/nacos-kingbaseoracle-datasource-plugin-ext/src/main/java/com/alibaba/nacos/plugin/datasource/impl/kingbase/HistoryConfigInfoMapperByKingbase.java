0453a3232edcefcefb905a5c2e5a1778f63087c2
zbw1231 <2313386675@qq.com>
2025-09-19 17:09:55 +0800

    nacos-plugin-sqlserver插件2.4.3且增加oracle分支

--- nacos-datasource-plugin-ext/nacos-kingbaseoracle-datasource-plugin-ext/src/main/java/com/alibaba/nacos/plugin/datasource/impl/kingbase/HistoryConfigInfoMapperByKingbase.java
+++ nacos-datasource-plugin-ext/nacos-kingbaseoracle-datasource-plugin-ext/src/main/java/com/alibaba/nacos/plugin/datasource/impl/kingbase/HistoryConfigInfoMapperByKingbase.java
@@ -16,9 +16,13 @@
 
 package com.alibaba.nacos.plugin.datasource.impl.kingbase;
 
+import com.alibaba.nacos.common.utils.CollectionUtils;
 import com.alibaba.nacos.plugin.datasource.constants.DatabaseTypeConstant;
+import com.alibaba.nacos.plugin.datasource.constants.FieldConstant;
 import com.alibaba.nacos.plugin.datasource.constants.PrimaryKeyConstant;
 import com.alibaba.nacos.plugin.datasource.impl.mysql.HistoryConfigInfoMapperByMySql;
+import com.alibaba.nacos.plugin.datasource.model.MapperContext;
+import com.alibaba.nacos.plugin.datasource.model.MapperResult;
 
 /**
  * The kingbase implementation of HistoryConfigInfoMapper.
@@ -32,6 +36,12 @@ public class HistoryConfigInfoMapperByKingbase extends HistoryConfigInfoMapperBy
         return DatabaseTypeConstant.KINGBASE;
     }
     public String getFunction(String functionName) {return functionName;}
+    @Override
+    public MapperResult removeConfigHistory(MapperContext context) {
+        String sql = "DELETE FROM his_config_info WHERE gmt_modified < ? and rownum <= ？";
+        return new MapperResult(sql, CollectionUtils.list(context.getWhereParameter(FieldConstant.START_TIME),
+                context.getWhereParameter(FieldConstant.LIMIT_SIZE)));
+    }
 
 
 
